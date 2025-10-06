d07a42ca43edd620bcfee8108b440b0cdf867bec
TheCharlatan <seb.kung@gmail.com>
2025-10-06 11:09:14 +0200

    Add sans utxo set block validation

--- src/validation.h
+++ src/validation.h
@@ -1034,6 +1034,10 @@ public:
      */
     void CheckBlockIndex() const;
 
+    BlockValidationState ValidateBlock(
+        const CBlock& block,
+        CBlockUndo& block_undo);
+
     /**
      * Alias for ::cs_main.
      * Should be used in new code to make it easier to make ::cs_main a member
